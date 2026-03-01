e221f7b92b68e2889f1c886d0ed9201f716c29fc
mahin527 <hasan.mahin527@gmail.com>
2026-03-01 17:57:43 +0600

    some files added

---
+++ 01_basic_lesson/07_hooks/src/App.jsx
@@ -0,0 +1,11 @@
+import Index from "./UseReducer/Index"
+
+function App() {
+  return (
+    <>
+    <Index />
+    </>
+  )
+}
+
+export default App
---
+++ 01_basic_lesson/07_hooks/src/UseReducer/Index.jsx
@@ -0,0 +1,19 @@
+
+const counterReducer = (state, action) => {
+    switch(action.type){
+        case 'Increament' :
+           
+    }
+}
+
+function index() {
+
+
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default index
